a4d443361cdf93858b35329055767728562d4ad5
Alv Gunnar Olof Romell <alr@netcompany.com>
2024-12-07 10:53:05 +0100

    Added solutions for day 1 and 2

--- AdventOfCodeFoundation/Solvers/2024/Day1Solver2024.cs
+++ AdventOfCodeFoundation/Solvers/2024/Day1Solver2024.cs
@@ -1,30 +1,57 @@
 ﻿using AdventOfCodeFoundation.IO;
+using System.Linq;
+using System.Numerics;
 
 namespace AdventOfCodeFoundation.Solvers._2024
 {
-    [Solves("2015/12/1")]
+    [Solves("2024/12/1")]
     internal class Day1Solver2024 : ISolver
     {
         public async Task<string> SolvePartOne(Input input)
         {
-            return (await input.GetRawInput()).Aggregate(0, (s, c) => s + ParseChar(c)).ToString();
+            bool debug = false;
+            var lines = await input.GetInputLines();
+            var X = new List<int>(); // Left column values
+            var Y = new List<int>(); // Right column values
+
+            foreach (var line in lines) {
+                X.Add(int.Parse(line.Split("   ")[0]));
+                Y.Add(int.Parse(line.Split("   ")[1]));
+            }
+            X.Sort();
+            Y.Sort();
+
+            var sortedPairs = X.Zip(Y);
+            var sum = 0;
+            foreach (var (First, Second) in sortedPairs)
+            {
+                if(debug) Console.WriteLine($"{First}, {Second}");
+                sum += Math.Abs(Second - First);
+            }
+
+            return sum.ToString();
         }
 
         public async Task<string> SolvePartTwo(Input input)
         {
-            var instructions = await input.GetRawInput();
-            var res = FindCellar(instructions);
+            var lines = await input.GetInputLines();
 
-            return res.HasValue ? $"{res.Value + 1}" : "No solution found.";
+            // Count occurrences for each number in the right column, store in a dict
+            // Loop through left column and multiply by occurences in the dict for that nr.
+
+            var X = new List<int>(); // List for left column values
+            var occurDict = new Dictionary<int, int>();
+
+            foreach (var line in lines)
+            {
+                X.Add(int.Parse(line.Split("   ")[0]));
+                var y = int.Parse(line.Split("   ")[1]);
+
+                occurDict[y] = occurDict.ContainsKey(y) ? occurDict[y] + 1 : 1;
+                
+            }
+            return X.Aggregate(0, (acc, curr) => acc + (curr * occurDict.GetValueOrDefault(curr,0))).ToString();
         }
 
-        private static int? FindCellar(string instructions)
-        {
-            var floor = 0;
-            var insts = instructions.TakeWhile(c => (floor += ParseChar(c)) >= 0).ToList();            
-            return floor < 0 ? insts.Count : null;
-        }
-
-        private static int ParseChar(char c) => c == '(' ? 1 : -1;
     }
 }
---
+++ AdventOfCodeFoundation/Solvers/2024/Day2Solver2024.cs
@@ -0,0 +1,92 @@
+﻿using AdventOfCodeFoundation.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCodeFoundation.Solvers._2024
+{
+    [Solves("2024/12/2")]
+    internal class Day2Solver2024 : ISolver
+    {
+        private bool _debug = false;
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var lines = await input.GetInputLines();
+
+            // For each line, check if it's an increasing or decreasing series, with a max difference of 3 between adjacent elements.
+            // Store number of safe and unsafe rows in variables or array?
+
+            var safeRows = 0;
+            var unsafeRows = 0;
+
+            foreach (var line in lines)
+            {
+                var lineArr = line.Split(' ').Select(c => int.Parse(c)).ToArray();
+                if (determineIfSafeRow(lineArr).isSafe) safeRows++;
+                else unsafeRows++;
+            }
+            if (_debug) Console.WriteLine($"safeRows: {safeRows}, unsafeRows: {unsafeRows}, totalRows:{lines.Count()}");
+
+            return safeRows.ToString();
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var lines = await input.GetInputLines();
+
+            // For each line, check if it's an increasing or decreasing series, with a max difference of 3 between adjacent elements.
+
+            var safeRows = 0;
+            var unsafeRows = 0;
+
+            foreach (var line in lines)
+            {
+                if (_debug) Console.WriteLine($"-- Original line: {line}");
+
+                var lineArr = line.Split(' ').Select(c => int.Parse(c)).ToArray();
+                var res = determineIfSafeRow(lineArr);
+                if (res.isSafe) safeRows++;
+                else if ((determineIfSafeRow(lineArr.Where((item, index) => index != res.unsafeIndex-1).ToList().ToArray()).isSafe) ||
+                        (determineIfSafeRow(lineArr.Where((item, index) => index != res.unsafeIndex).ToList().ToArray()).isSafe) ||
+                        (determineIfSafeRow(lineArr.Where((item, index) => index != res.unsafeIndex + 1).ToList().ToArray()).isSafe)) safeRows++;
+                else unsafeRows++;
+                if (_debug) Console.WriteLine($"----- Safe rows: {safeRows} -----");
+            }
+            if (_debug) Console.WriteLine($"safeRows: {safeRows}, unsafeRows: {unsafeRows}, totalRows:{lines.Count()}");
+
+            return safeRows.ToString();
+        }
+
+        private (bool isSafe,int unsafeIndex) determineIfSafeRow(int[] lineArr)
+        {
+            bool asc = lineArr[1] > lineArr[0];
+            bool desc = lineArr[1] < lineArr[0];
+
+            if (!asc && !desc) { if (_debug) Console.WriteLine($"line: {string.Join(" ", lineArr)}, safe?: {false}"); return (false, 0); };
+            bool isSafe = true;
+            var unsafeChar = 0;
+            if(_debug) Console.WriteLine($"Line: {string.Join(", ", lineArr)}, asc? {asc}, desc? {desc}");
+
+            for (int i = 0; i < lineArr.Length - 1; i++)
+            {
+                if (asc && (lineArr[i + 1] <= lineArr[i] || lineArr[i + 1] > lineArr[i] + 3))
+                {
+                    isSafe = false;
+                    unsafeChar = i;
+                    if (_debug) Console.WriteLine($"Asc Unsafe! currentNr: {lineArr[i]}");
+                    break;
+                }
+                else if (desc && (lineArr[i + 1] >= lineArr[i] || lineArr[i + 1] < lineArr[i] - 3))
+                {
+                    isSafe = false;
+                    unsafeChar = i;
+                    if (_debug) Console.WriteLine($"Desc Unsafe! currentNr: {lineArr[i]}");
+                    break;
+                }
+            }
+            if (_debug) Console.WriteLine($"line: {string.Join(" ", lineArr)}, safe?: {isSafe}");
+            return (isSafe, unsafeChar);
+        }
+
+    }
+}
